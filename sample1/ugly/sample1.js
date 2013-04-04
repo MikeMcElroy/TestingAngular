c6a5af9c34de2303e3ef4783e47eecaa91da9fdb
MikeMac <mikemac@jobvite.com>
2013-04-04 12:32:10 -0700

    Minor fix to remove localhost-coupling

--- sample1/ugly/sample1.js
+++ sample1/ugly/sample1.js
@@ -2,7 +2,7 @@ document.domain="localhost";
 angular.module("CodeExample1", [])
 	.controller("PeopleController", ["$scope", "$http", function($scope, $http) {
 		$scope.getPerson = function(id) {
-			$http.get("http://localhost:3001/url/people/" + id).success(function(results) {
+			$http.get("/url/people/" + id).success(function(results) {
 				$scope.person = results;
 			}).error(function(reason) {
 				$scope.showNoPerson = true;
@@ -10,7 +10,7 @@ angular.module("CodeExample1", [])
 		};
 
 		$scope.getConnectedPeople = function(id) {
-			$http.get("http://localhost:3001/url/people/"+ id +"/connected").success(function(results) {
+			$http.get("/url/people/"+ id +"/connected").success(function(results) {
 				$scope.connectedPeople = results;
 				if (results.length > 0) {
 					$scope.noConnectedPeople = false;
