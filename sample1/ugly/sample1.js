3181d6a48d846649a086770181138549db2c6dde
MikeMac <mikemac@jobvite.com>
2013-04-04 08:32:00 -0700

    More complete starting point.

--- sample1/ugly/sample1.js
+++ sample1/ugly/sample1.js
@@ -9,6 +9,19 @@ angular.module("CodeExample1", [])
 			});
 		};
 
+		$scope.getConnectedPeople = function(id) {
+			$http.get("http://localhost:3001/url/people/"+ id +"/connected").success(function(results) {
+				$scope.connectedPeople = results;
+				if (results.length > 0) {
+					$scope.noConnectedPeople = false;
+				} else {
+					$scope.noConnectedPeople = true;
+				}
+			}).error(function(reason) {
+				$scope.noConnectedPeople = true;
+			});
+		};
+
 		$scope.showNoPerson = false;
 
 	}]);
--- server.js
+++ server.js
@@ -1,32 +1,57 @@
-var exp = require('express');
-var app = exp();
+var exp = require('express'),
+	app = exp(),
+	data = {
+		"123": {id: "123", name: "Paul Paulenson"},
+		"321": {id: "321", name: "Robert Robertson"},
+		"432": {id: "432", name: "Joseph Josephson"},
+		"234": {id: "234", name: "Sarah Sarandaughter"}
+	},
+	connectedData = {
+		"123": ["321"],
+		"321": ["432", "123", "234"],
+		"432": [],
+		"234": ["123", "432"]
+	},
+	staticFiles = [
+		"/sample1/ugly/sample1.js",
+		"/sample1/bad/sample1.js",
+		"/sample1/good/sample1.js",
+		"/sample1/ugly/sample1.html"
+	],
+	i = 0,
+	l = staticFiles.length;
 
-var data = {
-	123: {id: 123, name: "Paul Paulenson"},
-	321: {id: 321, name: "Robert Robertson"},
-	432: {id: 432, name: "Joseph Josephson"},
-	234: {id: 234, name: "Sarah Sarandaughter"}
-};
+// app.get("/url/people/123", function(req, res) {
+// 	res.setHeader("Content-Type", "text/plain");
+// 	res.send(data[123]);
+// });
+// app.get("/url/people/321", function(req, res) {
+// 	res.setHeader("Content-Type", "text/plain");
+// 	res.send(data[321]);
+// });
+// app.get("/url/people/432", function(req, res) {
+// 	res.setHeader("Content-Type", "text/plain");
+// 	res.send(data[432]);
+// });
 
-app.get("/url/people/123", function(req, res) {
+app.get("/url/people/:id", function(req, res) {
 	res.setHeader("Content-Type", "text/plain");
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.send(data[123]);
+	res.send(data[req.params.id]);
 });
-app.get("/url/people/321", function(req, res) {
+
+app.get("/url/people/:id/connected", function(req, res) {
+	var to_send = [],
+		connections = connectedData[req.params.id],
+		i = 0, l = connections.length;
+
+	for( ; i < l ; i++) {
+		to_send.push(data[connections[i]]);
+	}
+
 	res.setHeader("Content-Type", "text/plain");
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.send(data[321]);
+	res.send(to_send);
 });
-app.get("/url/people/432", function(req, res) {
-	res.setHeader("Content-Type", "text/plain");
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.send(data[432]);
-});
-app.get("/url/people/234", function(req, res) {
-	res.setHeader("Content-Type", "text/plain");
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.send(data[234]);
-});
+
+app.use(exp.static(__dirname));
 
 app.listen(3001);
